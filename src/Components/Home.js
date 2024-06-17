921e3664ade09674a579d14a6434d1688450b483
angelinjebaj <angelinjebaj@gmail.com>
2024-06-17 07:08:10 +0530

    reacter_eg_commit

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './navbar/navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h1> hey welcome to homepage dear</h1>
+     
+    </div>
+  )
+}
+
+export default Home
